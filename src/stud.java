d69870fea5311069afb5f19911bb269be22f599d
rodolfobrian <rodolfo_bdf@hotmail.com>
2014-11-05 17:49:43 +0000

    Hello my .... bla bla

---
+++ src/stud.java
@@ -0,0 +1,21 @@
+
+public class stud {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		student s1 = new student(24, 'M');
+		student s2 = new student(26, 'M');
+		student s3 = new student(24, 'M');
+		student s4 = new student(24, 'M');
+		
+		s1.printingStudent();
+		s2.printingStudent();
+		
+
+	}
+
+}
---
+++ src/student.java
@@ -0,0 +1,24 @@
+
+public class student {
+
+	
+	private String name;
+	private int  age;
+	private char gender;
+	private int module;
+	
+	public student(int age, char gender){
+		this.age  = age;
+		this.gender = gender;
+		
+		
+	}
+	
+	public void printingStudent(){
+		System.out.println("the age is "+ age + "Gender is " + gender );
+	}
+	
+	
+	
+	
+}
